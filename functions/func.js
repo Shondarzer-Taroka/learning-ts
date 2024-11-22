6ad61c5a7485d43d841baf0f81479fea1119ff0b
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2024-11-22 17:42:58 +0600

    function in ts

--- functions/func.js
+++ functions/func.js
@@ -8,6 +8,24 @@ function hwel(a, b) {
 function optionalProperty(a, b, c) {
     return "".concat(c + ' ' + (a + b));
 }
-console.log(optionalProperty(99, 6565, 'add'));
+// // rest parameteres
+function resLearning() {
+    var numbers = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        numbers[_i] = arguments[_i];
+    }
+    return numbers.reduce(function (total, num) { return total + num; }, 0);
+}
+console.log(resLearning(2, 3, 4, 5, 6, 787));
+function getInfo(value) {
+    if (typeof value === "string") {
+        return "Name: ".concat(value);
+    }
+    else {
+        return "Age: ".concat(value);
+    }
+}
+console.log(getInfo("Alice")); // Output: Name: Alice
+console.log(getInfo(25)); // Output: Age: 25
 // console.log(troli('kkk','opoo'));
 // console.log(hwel('poli','shiio'));
--- functions/func.ts
+++ functions/func.ts
@@ -1,18 +1,57 @@
-let troli=(a:string,b:string):string=>{
-return a+b
+let troli = (a: string, b: string): string => {
+    return a + b
 }
 
-function hwel(a:string,b:string):string {
-    return a+b
+function hwel(a: string, b: string): string {
+    return a + b
 }
 
 // // optional property
 
-function optionalProperty(a:number,b:number,c:string):string {
-    return `${c+' '+(a+b)}`
+function optionalProperty(a: number, b: number, c: string): string {
+    return `${c + ' ' + (a + b)}`
 }
 
-console.log(optionalProperty(99,6565,'add'));
+
+// // rest parameteres
+
+function resLearning(...numbers: number[]): number {
+    return numbers.reduce((total, num) => total + num, 0)
+}
+
+console.log(resLearning(2, 3, 4, 5, 6, 787));
+
+
+// // overloading
+
+// function getInfo(name: string): string;
+// function getInfo(age: number): number;
+
+// function getInfo(value: string | number): string {
+//     if (typeof value === 'string') {
+//         return `Name: ${value}`;
+//     }
+
+//     else {
+//         return `Age: ${value}`;
+//     }
+
+// }
+// console.log(getInfo("Alice"));
+
+function getInfo(name: string): string;
+function getInfo(age: number): string;
+function getInfo(value: string | number): string {
+  if (typeof value === "string") {
+    return `Name: ${value}`;
+  } else {
+    return `Age: ${value}`;
+  }
+}
+
+console.log(getInfo("Alice")); // Output: Name: Alice
+console.log(getInfo(25));      // Output: Age: 25
+
 
 
 // console.log(troli('kkk','opoo'));
