0102b6ccae5b4126cc0279f22f4ff3ef18920b1c
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2024-11-22 18:33:00 +0600

    getlength in cast

--- casting/cast.js
+++ casting/cast.js
@@ -1,3 +1,17 @@
 var firstName = 'hello';
 var firstNameLength = firstName.length;
 console.log(firstNameLength);
+// // Using Angle Brackets Syntax
+var secondName = 'boli bol';
+var secondNameLength = firstName.length;
+// // Note: Avoid angle brackets (<type>) in JSX because they conflict with HTML.
+// const input = document.getElementById('myInput') as HTMLInputElement
+// input.value='alklk'
+// // Narrowing a Union Type
+function getLength(value) {
+    if (typeof value === 'string') {
+        return value.length;
+    }
+    return value.toString().length;
+}
+console.log(getLength(1223344333));
--- casting/cast.ts
+++ casting/cast.ts
@@ -3,3 +3,22 @@ let firstName:unknown='hello'
 let firstNameLength:number=( firstName as string).length
 console.log(firstNameLength);
 
+// // Using Angle Brackets Syntax
+
+let secondName:unknown='boli bol'
+
+let secondNameLength:number=(<string>firstName).length
+// // Note: Avoid angle brackets (<type>) in JSX because they conflict with HTML.
+// const input = document.getElementById('myInput') as HTMLInputElement
+// input.value='alklk'
+
+// // Narrowing a Union Type
+
+function getLength(value:string|number):number {
+    if ( typeof value==='string') {
+        return (value as string).length;
+    }
+
+    return value.toString().length
+}
+console.log(getLength(1223344333));
